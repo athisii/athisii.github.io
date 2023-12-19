105d85af85232c3521d01b8fe1627ad7e6194236
athisii <daiho.ekhe@gmail.com>
2023-12-19 21:55:25 +0530

    setup react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './loading.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Work in progress. Thanks for checking.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
